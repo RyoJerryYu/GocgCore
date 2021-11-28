6e40a13a23e5ad37cb695a81b19f67bb1dfa39fd
RyoJerryYu <tzl19961216@gmail.com>
2021-11-28 14:58:59 +0800

    feat(duel): add init codes

---
+++ duel/duel.go
@@ -0,0 +1,28 @@
+package duel
+
+import (
+	"github.com/RyoJerryYu/GocgCore/card"
+	"github.com/RyoJerryYu/GocgCore/effect"
+	"github.com/RyoJerryYu/GocgCore/field"
+	"github.com/RyoJerryYu/GocgCore/group"
+)
+
+type Interpreter struct{}
+
+// type cardSet map[*card.Card]struct{} // TODO: comparator
+
+type Duel struct {
+	StrBuffer string
+	Buffer    [0x1000]byte
+	BufferLen uint32
+	BufferP   *byte
+	Lua       Interpreter
+	GameField *field.Field
+	Random    Randomer
+	Cards     map[*card.Card]struct{}
+	Assumes   map[*card.Card]struct{}
+	Groups    map[*group.Group]struct{}
+	SGroups   map[*group.Group]struct{}
+	Effects   map[*effect.Effect]struct{}
+	UnCopy    map[*effect.Effect]struct{}
+}
